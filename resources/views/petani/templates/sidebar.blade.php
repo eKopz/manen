35d8295f5800824f9e88f27267ece979ff2e06b5
aldinaufal21 <ry.prg75@gmail.com>
2021-03-07 21:58:31 +0700

    add template admin petani

---
+++ resources/views/petani/templates/sidebar.blade.php
@@ -0,0 +1,77 @@
+<div class="nk-nav-scroll">
+    <ul class="metismenu" id="menu">
+        <li class="nav-label" style="color: #3d8b40">Menu</li>
+        <li class="mega-menu mega-menu-lg">
+            <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                <i class="fa fa-dashboard"></i><span class="nav-text">Dashboard</span>
+            </a>
+        </li>
+        <li class="mega-menu mega-menu-lg">
+            <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                <i class="fa fa-users"></i><span class="nav-text">Pengurus</span>
+            </a>
+            <ul aria-expanded="false">
+                <li><a href="/pengurus">Pengurus Koperasi</a></li>
+            </ul>
+        </li>
+        <li class="mega-menu mega-menu-lg">
+            <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                <i class="fa fa-user"></i><span class="nav-text">Anggota</span>
+            </a>
+            <ul aria-expanded="false">
+                <li><a href="/anggota">Anggota Koperasi</a></li>
+            </ul>
+        </li>
+        <li class="mega-menu mega-menu-lg">
+            <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                <i class="fa fa-money"></i><span class="nav-text">Simpanan</span>
+            </a>
+            <ul aria-expanded="false">
+                <li><a href="/simpanan/transaksi">Transaksi</a></li>
+                <li><a href="/simpanan">Rekap Simpanan</a></li>
+            </ul>
+        </li>
+        <li class="mega-menu mega-menu-lg">
+            <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                <i class="fa fa-money"></i><span class="nav-text">Pinjaman</span>
+            </a>
+            <ul aria-expanded="false">
+                <li><a href="/pinjaman/transaksi">Transaksi</a></li>
+                <li><a href="/pinjaman/pengajuan">Pengajuan Pinjaman</a></li>
+                <li><a href="/pinjaman/rekap">Rekap Simpanan</a></li>
+            </ul>
+        </li>
+        <li class="mega-menu mega-menu-lg">
+            <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                <i class="fa fa-bar-chart"></i><span class="nav-text">SHU</span>
+            </a>
+            <ul aria-expanded="false">
+                {{-- <li><a href="/keuangan/penghasilan/">Penghasilan saya</a></li>
+                <li><a href="/keuangan/rekening/">Rekening Bank</a></li> --}}
+                <li><a href="#">Coming Soon</a></li>
+            </ul>
+        </li>
+        <li class="mega-menu mega-menu-lg">
+            <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                <i class="fa fa-shopping-cart"></i><span class="nav-text">Kopmart</span>
+            </a>
+            <ul aria-expanded="false">
+                {{-- <li><a href="/keuangan/penghasilan/">Penghasilan saya</a></li>
+                <li><a href="/keuangan/rekening/">Rekening Bank</a></li> --}}
+
+                <li><a href="#">Coming Soon</a></li>
+            </ul>
+        </li>
+        <li class="mega-menu mega-menu-lg">
+            <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                <i class="fa fa-cog"></i><span class="nav-text">Pengaturan</span>
+            </a>
+            <ul aria-expanded="false">
+                {{-- <li><a href="/keuangan/penghasilan/">Penghasilan saya</a></li>
+                <li><a href="/keuangan/rekening/">Rekening Bank</a></li> --}}
+
+                <li><a href="/karyawan">Kelola Karyawan</a></li>
+            </ul>
+        </li>
+    </ul>
+</div>
